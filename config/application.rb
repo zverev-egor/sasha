b431d85b6c0c1e313826c106a6db1a6cd2594464
zverev-egor <zverev-egor@yandex.ru>
2015-09-02 13:28:40 +0300

    Add precompiled assets for Heroku

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,9 @@ module EduOnline
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = "Moscow"
-
+    config.serve_static_assets = false
+    I18n.enforce_available_locales = true
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
