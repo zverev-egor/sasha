e435fe9afb3793da059df62e3bd5d72fc94be803
zverev-egor <zverev-egor@yandex.ru>
2015-09-04 15:38:25 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,28 @@
 Rails.application.routes.draw do
-
-  resources :welcomes
   resources :users
+  resources :user_sessions
+  resources :welcomes
+
   # resources :sessions, only: [:new, :create, :destroy]
   root 'welcomes#index'
 
-  match '/new',    to: 'user_sessions#new',    via: 'get'
+  # match '/new',    to: 'user_sessions#new',    via: 'get'
+  #
+  # match '/create',    to: 'user_sessions#create',    via: 'get'
+  #
+  # match '/destroy',    to: 'user_sessions#destroy',    via: 'get'
 
-  match '/create',    to: 'user_sessions#create',    via: 'get'
+  get 'user_sessions/new'
 
-  match '/destroy',    to: 'user_sessions#destroy',    via: 'get'
+  get 'user_sessions/create'
+
+  get 'user_sessions/destroy'
 
   get 'login' => 'user_sessions#new', :as => :login
-
   get 'logout' => 'user_sessions#destroy', :as => :logout
-  resources :user_sessions
+
+
+
   #
   # get 'user_sessions/new'
   #
